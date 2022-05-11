37306b2a5572568af9d64626e623ec6b7ebd0463
Debjit Pal <88095103+debjitpal6@users.noreply.github.com>
2022-05-11 15:34:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+const X_CLASS='x';
+const O_CLASS='o';
+const cellElements = document.querySelectorAll('[data-cell]');
+const board=document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let oTurn;
+const WINNING_COMBINATIONS=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+startGame();
+
+restartButton.addEventListener('click',startGame);
+
+function startGame(){
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click',handleClick);
+        cell.addEventListener('click',handleClick,{once:true})
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e){
+    const cell= e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if(checkWin(currentClass)){
+        endGame(false);
+    }else if(isDraw()){
+        endGame(true);
+    }else{
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function isDraw(){
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS) ;
+    })
+}
+
+function endGame(draw){
+    if(draw){
+        winningMessageTextElement.innerText = 'Draw!';
+    }else{
+        winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Win!`;
+    }
+    winningMessageElement.classList.add("show");
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
+}
+
+function swapTurns(){
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if(oTurn){
+        board.classList.add(O_CLASS);
+    }else{
+        board.classList.add(X_CLASS);
+
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
